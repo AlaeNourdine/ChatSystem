9c076712a30a7bc4cc042e5764316e981f58d42d
AlaeNourdine <nourdine@etud.insa-toulouse.fr>
2022-01-25 03:08:40 +0100

    Major changes on the Messages format (maybe our final version) + methods in User

--- src/main/java/Model/Messages.java
+++ src/main/java/Model/Messages.java
@@ -2,37 +2,110 @@ package Model;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 
 public class Messages implements Serializable{
-
-
-
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
-	private final String msg;
-    private final Date horodatation;
-    private final User emetteur;
+	private User destinataire ;
+	private String msg;
+    private Date horodatage;
+    private User emetteur;
+    private int type ;
 
-    public Messages(String message, User emetteur)
+    public Messages ( String msg, User destinataire,User emetteur,int type) {
+        this.setDestinataire(destinataire) ;
+        this.setEmetteur(emetteur) ;
+        this.setMsg(msg) ;
+        this.setHorodatation(new Date());
+        this.type = type;
+    }
+
+    public void setDate(Date date) {
+        this.setHorodatation(date);
+    }
+
+    public String msgtoString() {
+        String msg = "";
+        msg += ("Destinataire::" + this.getDestinataire() + "\n") ;
+        msg += ("Emetteur::" + this.getEmetteur()+ "\n") ;
+        msg += ("Type::"+ this.type+ "\n");
+        msg += ("Date::" + this.horodateToString() + "\n") ;
+        msg += ("Message::" + this.getMsg() + "\n" );
+        return msg ;
+    }
+    
+    public static Messages stringToMessageHorodated(String s) {
+        String[] mots = s.split("\n");
+        User destinataire = User.stringToUser(mots[0].split("::")[1]);
+        User emetteur = User.stringToUser(mots[1].split("::")[1]);
+        int type = Integer.parseInt(mots[2].split("::")[1]);
+        String contenu = "";
+        for(int i=4; i< mots.length; i++) {
+            if(mots[i].startsWith("Message::")) {
+                mots[i]=mots[i].split("::")[1];
+            }
+            contenu += mots[i]+"\n";
+        }
+        return new Messages(contenu, destinataire, emetteur, type);
+    }
+    
+    public String horodateToString() {
+        DateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        return format.format(this.getHorodatation());
+    }
+    
+    
+	private void setHorodatation(Date horodatage) {
+		this.horodatage = horodatage ;
+	}
+
+    public Date getHorodatation()
     {
-        this.msg = message;
-        this.horodatation = new Date();
-        this.emetteur = emetteur;
+        return this.horodatage;
 
     }
 
-    public String getMessage()
+    
+	private void setMsg(String msg) {
+		this.msg = msg;
+		
+	}
+	
+	public String getMsg()
     {
         return this.msg;
 
     }
-
-    public Date getHorodatation()
-    {
-        return this.horodatation;
-
-    }
+	
+	
+	private void setEmetteur(User emetteur) {
+		this.emetteur = emetteur;
+		
+	}
 
     public User getEmetteur() {
         return emetteur;
     }
+    
+    private User getDestinataire() {
+    	return this.destinataire;
+    }
+	private void setDestinataire(User destinataire) {
+		this.destinataire = destinataire ;
+		
+	}
+	
+    public int getType() {
+        return type;
+    }
+    
+    public void setType(int type) {
+        this.type = type;
+    }
+
 }
--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,22 +1,22 @@
 package Model;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class User {
 	private final InetAddress ipAddress;
     private String nickname;
     private Integer port ; 
     
-    //Integer TAILLE_MAX = 10; 
+ 
     
     public User (String nickname, InetAddress ipAddress, Integer port){
 
         this.setNickname(nickname);
         this.setPort(port);
         this.ipAddress= ipAddress;
-        
     }
-    
+        
     private Integer getPort() {
     	return port;
     }
@@ -35,6 +35,31 @@ public class User {
     public void setNickname(String nickname) {
         this.nickname = nickname;
     }
+    
+    
+    public String UserToString(){
+        String str = "";
+        str+="nickname" + this.nickname + " - ";
+        str+="port " + (this.port).toString() + " - ";
+        str+="ipAddress " + (this.ipAddress).toString() + " - ";
+        return str;
+    }
+    
+    public static User stringToUser(String userstr) {
+        String nickname;
+        Integer port = 0;
+        String ip = "" ;
+        String mots[] = userstr.split(" ");
+        nickname=mots[1];
+        port=Integer.parseInt(mots[4]);
+        ip=mots[7];
+        User user = null;
+        try {
+            user = new User(nickname,InetAddress.getByName(ip.split("/")[1]), port);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
 
-   
 }

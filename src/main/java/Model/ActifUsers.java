8aab6deff9f4b50bb60230038040f2bb78565031
AlaeNourdine <nourdine@etud.insa-toulouse.fr>
2022-01-25 02:06:59 +0100

    ActifUsers and UserHandler fusion

--- src/main/java/Model/ActifUsers.java
+++ src/main/java/Model/ActifUsers.java
@@ -35,11 +35,39 @@ public class ActifUsers {
 				return elem ; 
 			}
 		}
-		throw new Exception("Cette adresse IP ne correspond à aucun utilisateurs.");
+		throw new Exception("Cette adresse IP ne correspond à aucun utilisateur.");
 		
 	}
 	
+	public synchronized void deleteUser(User user) {
+		boolean Sup = false ;
+		for(User elem: this.actifUsersList)
+		{
+			if (elem.equals(user)) {
+				this.actifUsersList.remove(elem);
+				Sup = true ;
+				this.showActifUsers();
+				break;
+			}
+		}
+		if (!Sup) {
+            System.out.println("Utilisateur n'est pas dans la liste.");
+        }
+	}
 	
+    public synchronized void editList(String ancien , String nouveau) {
+        for(User elem: this.actifUsersList)
+        {
+            if (ancien.equals( elem.getNickname() ) ) {
+                this.actifUsersList.remove(elem);
+                elem.setNickname(nouveau);
+                this.ajouterUser(elem);
+                break ;
+            }
+        }
+        this.showActifUsers();
+    }
+
 	public Boolean appartient(User user) {
         boolean appartient = false ;
         for(User elem: this.actifUsersList)
@@ -52,4 +80,24 @@ public class ActifUsers {
         System.out.println("appartient ?: " + appartient);
         return appartient;
     }
+	
+	
+    public void showActifUsers() {
+        System.out.println ("Utilisateurs actifs : ");
+        StringBuilder User = new StringBuilder();
+        for(User elem: this.actifUsersList)
+        {
+            System.out.println (elem.toString());
+            User.append(elem).append("\n");
+        }
+    }
+        public Boolean checkNicknameUnicity(String nickname) {
+            for(User elem: this.actifUsersList)
+            {
+                if (nickname.equals( elem.getNickname() ) ) {
+                    return false ;
+                }
+            }
+            return true;
+        }
 }

263b1735c92ab399516dc4af5c968a7b524654cc
Twix02 <77344669+Twix02@users.noreply.github.com>
2022-01-26 08:12:49 +0100

    DataBase and Inteface

--- src/main/java/Network/Discussion.java
+++ src/main/java/Network/Discussion.java
@@ -1,4 +1,7 @@
-package Network;
+/*package Network;
+
+import Controller.Controller;
+import Model.*;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
@@ -8,17 +11,33 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 
-
-public Discussion(Socket link, ChatApp app) {
-    this.setLink(link);
-    this.setApp(app);
-    try {
-        this.setOut(new ObjectOutputStream(link.getOutputStream()));
-        this.setIn(new ObjectInputStream(link.getInputStream()));
-    }catch(Exception e) {
-        e.getStackTrace();
-    }
-    this.derniersMsg = new ArrayList<>();
-    this.pcs = new PropertyChangeSupport(this);
-    this.start();
+public class Discussion extends Thread {
+	
+	public Discussion(Socket link, Controller app) {
+	    this.setLink(link);
+	    this.setApp(app);
+	    try {
+	        this.setOut(new ObjectOutputStream(link.getOutputStream()));
+	        this.setIn(new ObjectInputStream(link.getInputStream()));
+	    }catch(Exception e) {
+	        e.getStackTrace();
+	    }
+	    this.derniersMsg = new ArrayList<>();
+	    this.pcs = new PropertyChangeSupport(this);
+	    this.start();
+	}
+	
+	
+	public Discussion(User u2, Controller app ) {
+		this.setU2(u2);
+		this.setApp(app);
+		try {
+			socket s = new socket(u2.getIpAddress(),5000);
+			this.setOut(new ObjectOutputStream)
+		}
+	}
+	public static void main(String[] args) {
+		
+	}
 }
+*/
--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -1,5 +1,61 @@
 package View;
 
-public class View {
+import java.net.URL;
 
+import Controller.Controller;
+import java.awt.*;
+import javax.swing.*;
+
+
+/** 
+ * Classe representant la page principale. 
+ */
+public class View extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7128287592232261943L;
+
+	public View() {
+		super ("ChatApp");
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setSize(600, 400);
+		this.setLocationRelativeTo(null);
+		
+		JPanel contentPane = (JPanel) this.getContentPane();
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Bienvenue !");
+		lblNewLabel.setBounds(225, 20, 160, 50);
+		contentPane.add(lblNewLabel);
+		
+		
+		JLabel lblNewLabel_1 = new JLabel("Choisis ton pseudo");
+		lblNewLabel_1.setBounds(200, 40, 160, 50);
+		contentPane.add(lblNewLabel_1);
+		
+		
+		
+		
+		JTextField pseudo = new JTextField();
+		pseudo.setText("Pseudo");
+		pseudo.setColumns(10);
+		pseudo.setBounds(200, 80, 160, 50);
+		contentPane.add(pseudo);
+		
+		JButton btnConnexion = new JButton("Connexion");
+		btnConnexion.setBounds(360, 90, 160, 30);
+		contentPane.add(btnConnexion);
+		
+	}
+
+	public static void main(String[] args) {
+		
+		
+		//Start View
+		View view = new View() ;
+		view.setVisible(true);
+		
+	}
 }

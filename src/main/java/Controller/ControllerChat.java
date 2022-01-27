772c457cf6bff411643ad2e6248f50083cbfd472
AlaeNourdine <nourdine@etud.insa-toulouse.fr>
2022-01-28 00:05:14 +0100

    Add mutliple methods to ControllerChat

--- src/main/java/Controller/ControllerChat.java
+++ src/main/java/Controller/ControllerChat.java
@@ -2,33 +2,32 @@ package Controller;
 
 import Model.ActifUsers;
 import Model.User;
-import Network.* ;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import Network.TCPReceive ;
+import Network.Discussion;
+import Network.broadcastUDP;
 
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.util.ArrayList;
-import java.util.Enumeration;
 
 
-public class ControllerChat {
+public class ControllerChat implements PropertyChangeListener{
 	
 	private ActifUsers actifs ;
-	private User me ;
+	private static User me ;
 	private static ControllerChat chatsystem ;
-	private boolean online ;
-	//private RunnerEcouteTCP runnerEcouteTCP;
+	private static boolean online ;
+	//private  TCPReceive listener;
 	public ArrayList<InetAddress> listCommunication = new ArrayList<>();
-	private JSONObject configJSON;
 	public static String FILE_SEPARATOR = System.getProperty("file.separator");
 	
+	
+	public static void main(String[] args) {
+		
+	}
 	
 	public boolean isOnline() {
 		return online ;
@@ -44,16 +43,6 @@ public class ControllerChat {
 
         String path = System.getProperty("user.dir")+ FILE_SEPARATOR + "config.json";
         System.out.println("Path is : "+path);
-        JSONParser jsonP = new JSONParser();
-        try {
-            this.configJSON = (JSONObject) jsonP.parse(new FileReader(path));
-        } catch (IOException e) {
-            System.out.println("Fichier JSON non trouvable");
-            e.printStackTrace();
-        } catch (ParseException e) {
-            System.out.println("Fichier JSON mal formé");
-            e.printStackTrace();
-        }
     }
     
     public static synchronized ControllerChat getInstance(String nickname, Integer port){
@@ -66,7 +55,83 @@ public class ControllerChat {
         return chatsystem;
     }
     
-	public User getMe() {
+    public boolean editNickname(String nouveau) throws IOException{
+        String broadcastMessage = "Demande Modification Pseudo\n" + getMe().toString() + "\n" + nouveau + "\n";
+        broadcastUDP.envoiBroadcast(broadcastMessage);
+        try {
+            Thread.sleep(2000);
+            
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (broadcastUDP.getvalidNickname()) {
+            System.out.println("Modification pseudo reussi");
+            broadcastMessage = "Modification pseudo reussi\n" + getMe().toString() + "\n" + nouveau + "\n";
+            broadcastUDP.envoiBroadcast(broadcastMessage);
+            this.getActifUsers().editList(getMe().getNickname(), nouveau);
+            getMe().setNickname(nouveau);
+            System.out.println("Changement pseudo accepte, nouvelle liste des utilisateurs actifs:");
+            this.getActifUsers().showActifUsers();
+            return true;
+        }
+        else
+        {
+            System.out.println("Echec Modification pseudo");
+            broadcastUDP.setvalidNickname(true);
+            return false;
+        }
+    }
+    
+    public static boolean connexion(String nickname) throws IOException{
+    	me.setNickname(nickname);
+        String broadcastMessage = "Connexion\n" + getMe().toString() ;
+        broadcastUDP.envoiBroadcast(broadcastMessage);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (broadcastUDP.getConnexion()) {
+            System.out.println("Connexion reussie");
+            online=true;
+            return true;
+        }
+        else
+        {
+            System.out.println("Connexion echoue");
+            broadcastUDP.setConnexion(true);
+            return false ;
+        }
+    }
+
+  
+    public void demarrerSession(String nickname){
+        User user = this.actifs.getUserByNickname(nickname);
+        Discussion session = new Discussion(user,this);
+        //listener.addSession(session);
+    }
+    
+
+    public void propertyChange(PropertyChangeEvent evt) {
+        switch (evt.getPropertyName()){
+            case "chatCreated" :
+                //Discussion session = this.listener.getDiscussion();
+                //session.addPropertyChangeListener(this);
+                break;
+        }
+    }
+    
+    public void activerEcouteTCP(){
+        //this.listener = new listener(this);
+        //this.listener.addPropertyChangeListener(this);
+    }
+
+	public static User getMe() {
 		return me;
 	}
+	
+    public ActifUsers getActifUsers() {
+        return actifs;
+    }
+
 }

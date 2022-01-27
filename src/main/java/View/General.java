454b7b2a4c5a71b09fe1ca34ae309349738b2eca
Twix02 <77344669+Twix02@users.noreply.github.com>
2022-01-28 00:10:40 +0100

    add interface General

---
+++ src/main/java/View/General.java
@@ -0,0 +1,13 @@
+package View;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+
+import Model.*;
+import Network.* ;
+
+public class General {
+
+}

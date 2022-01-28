fcd740679cf64a8f650d620e3d967fe87911bb4f
Twix02 <77344669+Twix02@users.noreply.github.com>
2022-01-28 04:42:54 +0100

    View new version

--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -1,94 +1,3 @@
-/*package View;
-
-import java.net.URL;
-
-import Controller.Controller;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.net.SocketException;
-
-import Model.Messages;
-
-
-/** 
- * Classe representant la page principale. 
- */
-
-/*
-public class View extends JPanel implements ActionListener {
-	
-	
-	private static final long serialVersionUID = 7128287592232261943L;
-	private static String nickname ;
-	private ObjectOutputStream out ;
-	private String ipAddress ;
-	
-	private JLabel lblNewLabel_1 ;
-	private JPanel viewPanel ;
-	private static JFrame viewFrame ; 
-	
-	public View()  {
-		
-		
-		super ("ChatApp");
-		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.setSize(600, 400);
-		this.setLocationRelativeTo(null);
-		
-		//this.out = out ;
-		
-		//viewFrame = new JFrame ("View Window");
-		//viewPanel = new JPanel(new GridLayout (10,10)) ;
-
-		
-		JPanel contentPane = (JPanel) this.getContentPane();
-		contentPane.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("Bienvenue !");
-		lblNewLabel.setBounds(225, 20, 160, 50);
-		contentPane.add(lblNewLabel);
-		
-		
-		JLabel lblNewLabel_1 = new JLabel("Choisis ton pseudo");
-		lblNewLabel_1.setBounds(200, 40, 160, 50);
-		contentPane.add(lblNewLabel_1);
-		
-		
-		
-		
-		JTextField pseudo = new JTextField();
-		pseudo.setText("Pseudo");
-		pseudo.setColumns(10);
-		pseudo.setBounds(188, 82, 160, 30);
-		contentPane.add(pseudo);
-		
-		JButton btnConnexion = new JButton("Connexion");
-		btnConnexion.setBounds(356, 82, 160, 30);
-		contentPane.add(btnConnexion);
-	
-		btnConnexion.addActionListener(this);
-		contentPane.getRootPane().setDefaultButton(btnConnexion);
-		
-		
-}	
-	
-	public void actionPerformed(ActionEvent event) {
-		nickname = lblNewLabel_1.getText();
-		System.out.println("Login Username : " + nickname);
-	}
-	
-	public static void main (String[] args) {
-		View view = new View() ;
-		view.setVisible(true);
-	}
-	
-}
-*/
-
 package View;
 
 import java.net.URL;
@@ -105,8 +14,8 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.SocketException;
 
-import Model.Messages;
-
+import Model.*;
+import Controller.*;
 
 /** 
  * Classe representant la page principale. 
@@ -115,43 +24,21 @@ public class View {
 	
 	
 	private static final long serialVersionUID = 7128287592232261943L;
-	private static String nickname ;
+	private static String username ;
 	private ObjectOutputStream out ;
 	private String ipAddress ;
 	private JLabel lblNewLabel_1 ;
 	private JPanel viewPanel ;
 	private static JFrame viewFrame ; 
+	private JLabel msg;
 	
-	public View(String username) throws IOException  {
-		View.nickname = username ;
+	public View(String nickname) throws IOException  {
+		View.username = nickname ;
 
 		viewFrame = new JFrame(nickname) ;
 		viewFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		viewFrame.setSize(600, 400);
 		viewFrame.setLocationRelativeTo(null);
-		
-		JButton btnConnexion = new JButton("Connexion");
-		btnConnexion.setBounds(356, 82, 160, 30);
-		viewFrame.add(btnConnexion);
-	
-		btnConnexion.addActionListener(
-				new ActionListener() {
-					public void actionPerformed(ActionEvent MyActionEvent) {
-					//	ControllerChat.connexion(nickname);
-						viewFrame.setVisible(false);
-						new Clavardage(nickname);
-						
-					}
-					
-				}
-				);
-		viewFrame.getRootPane().setDefaultButton(btnConnexion);
-		
-		JTextField pseudo = new JTextField();
-		pseudo.setText("Pseudo");
-		pseudo.setColumns(10);
-		pseudo.setBounds(188, 82, 160, 30);
-		viewFrame.add(pseudo);
 		
 		JLabel lblNewLabel = new JLabel("Bienvenue !");
 		lblNewLabel.setBounds(225, 20, 160, 50);
@@ -159,32 +46,60 @@ public class View {
 		
 		
 		JLabel lblNewLabel_1 = new JLabel("Choisis ton pseudo");
-		lblNewLabel_1.setBounds(200, 40, 160, 50);
+		lblNewLabel_1.setBounds(200, 40, 160, 30);
 		viewFrame.add(lblNewLabel_1);
 		viewFrame.setVisible(true);
 		
+		JTextField pseudo = new JTextField();
+		pseudo.setText("Pseudo");
+		pseudo.setColumns(10);
+		pseudo.setBounds(188, 100, 160, 30);
+		viewFrame.add(pseudo);
+			
+	
+		JButton btnConnexion = new JButton("Connexion");
+		btnConnexion.setBounds(356, 100, 160, 30);
+		viewFrame.add(btnConnexion);
+	
+		btnConnexion.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent MyActionEvent) {
+						String nom = pseudo.getText(); //On récupère le pseudo choisi par l'utilisateur
+						boolean used;
+						try {
+							used = Manager.validUsername(nom);
+							if (! used) {
+								msg= new JLabel ("Pseudo invalide. Veuillez en choisir un autre.");
+								JOptionPane.showMessageDialog(viewFrame, msg);
+							}else {
+								viewFrame.setVisible(false);
+								General general = new General (nickname);
+							}
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+
+						
+					}
+					
+				}
+				);
+		viewFrame.getRootPane().setDefaultButton(btnConnexion);
+		
+
+
 		
 	}
-	
-		
-		//this.out = out ;
-		
-		//viewFrame = new JFrame ("View Window");
-		//viewPanel = new JPanel(new GridLayout (10,10)) ;
-
-
-
-		
-		
 
 
 	public void actionPerformed(ActionEvent event) {
-		nickname = lblNewLabel_1.getText();
-		System.out.println("Login Username : " + nickname);
+		username = lblNewLabel_1.getText();
+		System.out.println("Login Username : " + username);
 	}
 	
 	public static void main (String[] args) throws IOException {
-		new View(nickname) ;
+		new View(username) ;
 		
 	}
 	

755a86e2d0acadbf70ac4eb51be2740a072fd1bb
Twix02 <77344669+Twix02@users.noreply.github.com>
2022-01-28 04:44:06 +0100

    Exit window

---
+++ src/main/java/View/Exit.java
@@ -0,0 +1,61 @@
+package View;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.*;
+
+public class Exit {
+	private JFrame exitFrame ; 
+	
+	private JPanel exitPanel = new JPanel (new FlowLayout(FlowLayout.CENTER));
+	private JButton btnExit = new JButton ("Confirmer");
+	private JLabel exitLabel ;
+	
+	private static String username ;
+	
+	public Exit(String nickname) throws IOException {
+		username=nickname;
+		
+		exitFrame = new JFrame ("Modifier pseudo"); 	
+		
+		exitFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		exitFrame.setSize(600, 400);
+		exitFrame.setLocationRelativeTo(null);
+		exitFrame.setVisible(true);
+		
+		exitLabel = new JLabel ("Attention, vous allez changer votre pseudo, êtes-vous sûr?");
+		exitLabel.setBounds(188,82,160,30);
+		exitPanel.add(exitLabel);
+		btnExit.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent MyActionEvent) {
+						exitFrame.setVisible(false);
+						try {
+							General general = new General (nickname);
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+					}
+					
+				}
+				);
+		exitFrame.getRootPane().setDefaultButton(btnExit);
+
+		
+		btnExit.setBounds(188, 90, 160, 30);
+		exitPanel.add(btnExit);
+		
+	
+		exitFrame.add(exitPanel);
+		}
+	
+	public static void main (String[] args) throws IOException {
+		new Exit(username);	;
+	}
+}
